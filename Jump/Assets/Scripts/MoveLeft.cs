247c091cc4e3fa0bf6a19ab1ad391bf21ceb9731
Thalia <20241ctb0180018@estudantes.ifpr.edu.br>
2026-05-22 11:34:10 -0300

    Versão1
    Começo

---
+++ Jump/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 25;
+    private float leftBound = -10;
+    
+    //[SerializeField] private PlayerController playerController;
+
+    public void Init(PlayerController script)
+    {
+        playerController = script;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!PlayerController.IsGameOver())
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);    
+
+            if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Jump/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    [SerializeField] PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstable", startDelay, repeatRate);
+    }
+
+    void SpawnObstable ()
+    {
+        if (!PlayerController.IsGameOver()) {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    } 
+}
